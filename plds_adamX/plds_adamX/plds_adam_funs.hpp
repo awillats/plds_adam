1300205168d2e1301fce257f48292e9b2d988150
awillats <awillats3@gatech.edu>
2019-05-22 16:27:56 -0400

    editted default params for glds

--- plds_adamX/plds_adamX/plds_adam_funs.hpp
+++ plds_adamX/plds_adamX/plds_adam_funs.hpp
@@ -104,7 +104,7 @@ class glds_adam : public lds_adam{
 	adam::Mat Q;
 	adam::Mat R;
 
-         glds_adam(): lds_adam(), qmag(1e-1), rmag(1e-2)
+         glds_adam(): lds_adam(), qmag(1e-2), rmag(1e-1)
 	{
 	    initSys();
 	};
